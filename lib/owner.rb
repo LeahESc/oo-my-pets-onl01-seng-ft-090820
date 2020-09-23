065d7fe8e082a83e6bc41638e9bb1da68590aa45
Leah Schlackman <leahemily1@gmail.com>
2020-09-23 18:57:17 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -39,7 +39,7 @@ class Owner
   def buy_cat(cat)
     # binding.pry 
     cat = Cat.new(name, owner = Self, mood = "nervous") 
-    # cat.owner = Self
+     cat.owner = Self
     # self.cats << cat
     # binding.pry 
       # Cat.all.each do |cat| 
