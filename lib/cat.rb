f42a39456375803510fb95d5b705f30a84c8169d
Leah Schlackman <leahemily1@gmail.com>
2020-09-23 17:22:49 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,10 +1,11 @@
 class Cat
- attr_accessor  :owner
+ attr_accessor  :owner, :mood 
  attr_reader :name
  
   def initialize(name, owner, mood = "nervous")
    @name = name
    @owner = owner
-   @mood = "nervous"
+  # @mood = "nervous"
   end
+  
 end
