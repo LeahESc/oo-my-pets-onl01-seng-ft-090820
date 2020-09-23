6c90ee2beb556bfa6cdfbb389c89153ae902054c
Leah Schlackman <leahemily1@gmail.com>
2020-09-23 17:53:51 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -37,7 +37,7 @@ class Owner
   
   def buy_cat(cat)
     # binding.pry 
-    cat.owner = self 
+    Cat.owner = self 
     # Cat.all.each do |cat| 
     #   if cat.owner != self 
     self.cats << cat
