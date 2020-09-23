d10a9ee3dee44fd9edf5f0716ab4790657cd7607
Leah Schlackman <leahemily1@gmail.com>
2020-09-23 16:30:44 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,8 +1,9 @@
 class Cat
  attr_accessor :name, :owner
  
- def initialize(name, owner, mood = "nervous")
+  def initialize(name, owner, mood = "nervous")
    @name = name
    @owner = owner
    @mood = "nervous"
+  end
 end
